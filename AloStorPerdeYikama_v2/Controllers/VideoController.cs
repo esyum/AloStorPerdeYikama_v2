6fbfc4382ca3aca737dbd65dcbef3a2e29814a63
esyum <esyu79@gmail.com>
2020-02-26 01:23:11 +0300

    video to database

---
+++ AloStorPerdeYikama_v2/Controllers/VideoController.cs
@@ -0,0 +1,49 @@
+﻿using AloStorPerdeYikama_v2.Context;
+using AloStorPerdeYikama_v2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AloStorPerdeYikama_v2.Controllers
+{
+    public class VideoController : Controller
+    {
+        DatabaseContext db = new DatabaseContext();
+        // GET: Video
+        public ActionResult Index()
+        {
+           
+            return View(db.TblFile.Where(p=>p.ContentType=="video/mp4").ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            byte[] bytes;
+            using (BinaryReader br = new BinaryReader(postedFile.InputStream))
+            {
+                bytes = br.ReadBytes(postedFile.ContentLength);
+            }
+            
+            db.TblFile.Add(new tblFile
+            {
+                Name = Path.GetFileName(postedFile.FileName),
+                ContentType = postedFile.ContentType,
+                Data = bytes
+            });
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public FileResult DownloadFile(int? fileId)
+        {
+
+            tblFile file = db.TblFile.ToList().Find(p => p.ID == fileId.Value);
+            return File(file.Data, file.ContentType, file.Name);
+        }
+    }
+}
---
+++ AloStorPerdeYikama_v2/Models/tblFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AloStorPerdeYikama_v2.Models
+{
+    [Table("tblFile")]
+    public partial class tblFile
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string ContentType { get; set; }
+
+        public byte[] Data { get; set; }
+    }
+}
